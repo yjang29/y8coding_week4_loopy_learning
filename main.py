959ef9102496c285773366d810aaaa8d736b2b3e
yjang29 <YJang29@kellettschool.com>
2024-10-28 06:53:51 +0000

    save

--- main.py
+++ main.py
@@ -3,43 +3,63 @@
 ## *UNDERNEATH* EACH MESSAGE!
 
 print("\nTASK 1 HERE\n")
-
+for i in range(5):
+    print("bob")
 
 
 print("\nTASK 2 HERE\n")
-
-
+for j in range(7):
+    print("🍜noodles")
 
 print("\nTASK 3 HERE\n")
-
+for j in range(3):	
+	    print("🦖 Dinosaur")
+	    print("🐘 Elephant")   
 
 
 print("\nTASK 4 HERE\n")
+for x in range(6):	
+	    print("🐶 Dog") 
+print("🐱 Cat")
 
 
 
 print("\nTASK 5 HERE\n")
-
+print("🐁 mouse")
+for x in range(4):	
+	    print("🐸 Frog")
+print("🐎horse")	
 
 
 print("\nTASK 6 HERE\n")
-
+for x in range(2):	
+    print("🐶 Dog")
+    print("🐱 Cat")
+	
 
 
 print("\nTASK 7 HERE\n")
-
+print("🦒 Giraffe")
+for x in range(3):	
+	print("🐒 Monkey")
 
 
 print("\nTASK 8 HERE\n")
-
+print("🐯 Tiger")
+print("🦁 Lion")
+print("🐯 Tiger")
 
 
 print("\nTASK 9 HERE\n")
-
-
-
+print("🙈 Chimp")
+for x in range(3):	
+    for y in range(3):
+        print("🦔 Hedgehog")
+        print("🐮 Cow")
+    print("🦍 Gorilla")
 print("\nTASK 10 HERE\n")
 
+	
 
 
 print("\nTASK 11 HERE\n")
